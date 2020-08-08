71baf25d87f89c9e9dbfd394abd70d603b0e7ca9
Tahsin <tahsingenuies@gmail.com>
2020-08-09 03:27:19 +0700

    feat: 🎸 implement update checker

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ import ProcessAndPorts from "./operations/processAndPorts";
 import DockerTools from "./operations/docker";
 import Network from "./operations/network";
 import * as fuzzy from "fuzzy";
+import shell from "shelljs";
+import semver from "semver";
+import ora from "ora";
 
 const choices = [
   { title: "Process & Port Tools", value: "process-and-ports", description: "Tools related to port and process" },
@@ -31,12 +34,51 @@ const ask = async () => {
   return response;
 };
 
+const utils = {
+  getConfirmation(message: string) {
+    return prompts(
+      {
+        type: "confirm",
+        name: "confirmed",
+        message,
+        initial: true,
+      },
+      { onCancel: process.exit as any }
+    );
+  },
+};
+
 class CrossTools extends Command {
   async run() {
+    const spinner = ora("Loading unicorns");
+
     process.on("exit", () => {
       console.log(colors.cyan("Bye 👋"));
     });
 
+    const pkgName = "cross-tools";
+
+    const output = shell.exec("npm list -g --depth=0 --json", { silent: true }).stdout;
+    const installed = JSON.parse(output).dependencies[pkgName];
+    if (!installed) {
+      const { confirmed } = await utils.getConfirmation(`Do you want to create this package globally?
+      ${colors.yellow(`(By doing that so, you don't have to download it on every execution.)`)}`);
+    } else {
+      const output = shell.exec(`npm show ${pkgName} time --json`, { silent: true }).stdout;
+      const latestVersion = Object.keys(JSON.parse(output)).reverse()[0];
+      // const hasUpdaate = semver.gt(latestVersion, installed.version);
+      const hasUpdaate = true;
+      if (hasUpdaate) {
+        const { confirmed } = await utils.getConfirmation(`There is an update available. Do you want to update it now?`);
+        if (confirmed) {
+          spinner.text = `Updating ${pkgName}`;
+          spinner.start();
+          shell.exec(`npm i -g ${pkgName}`, { silent: true });
+          spinner.stop();
+          console.log(colors.green(`✅ ${pkgName} updated to v${latestVersion}. ${colors.yellow("(Will be affected next time)")}\n`));
+        }
+      }
+    }
     const { operation } = await ask();
 
     switch (operation) {
