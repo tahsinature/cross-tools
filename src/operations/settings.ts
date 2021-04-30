f6ec0e126dbc2c4345a2dda6180b74e8fe4342a3
Tahsin <tahsingenuies@gmail.com>
2021-05-01 05:10:07 +0700

    remove junk settings

--- src/operations/settings.ts
+++ src/operations/settings.ts
@@ -1,65 +1,19 @@
 import { Command } from '@oclif/command';
 import { askFuzzy } from '../util/myPrompts';
-import config from '@app/config';
-import prompts from 'prompts';
+import checkForUpdate from '@app/util/checkForUpdate';
 
-const choices = [
-  { title: 'Update check frequency', value: 'update-check-frequency' },
-  { title: 'Check for update', value: 'check-for-update' },
-  { title: 'Check for local installation', value: 'check-for-local-installation' },
-];
+const choices = [{ title: 'Check for update', value: 'check-for-update' }];
 
 class Settings extends Command {
   async run() {
+    console.clear();
     const { operation } = await askFuzzy(choices, {});
 
     switch (operation) {
-      case 'update-check-frequency':
-        const { updateCheckIntervalInDays } = await prompts(
-          {
-            type: 'number',
-            name: 'updateCheckIntervalInDays',
-            message: 'How often should check for updates? (For this option, check for local installation needs to be enabled)',
-            max: 7,
-            min: 0,
-            float: false,
-            initial: config.state.updateCheckIntervalInDays,
-          },
-          { onCancel: () => process.exit() }
-        );
-        config.update('updateCheckIntervalInDays', updateCheckIntervalInDays);
+      case 'check-for-update':
+        await checkForUpdate();
         await this.run();
         break;
-
-      case 'check-for-local-installation':
-        const { checkForLocalInstallationOnBoot } = await prompts(
-          {
-            type: 'confirm',
-            name: 'checkForLocalInstallationOnBoot',
-            message: 'During bootup checks if this app installed globally',
-            initial: config.state.checkForLocalInstallationOnBoot,
-          },
-          { onCancel: () => process.exit() }
-        );
-
-        config.update('checkForLocalInstallationOnBoot', checkForLocalInstallationOnBoot);
-        await this.run();
-        break;
-
-      // case 'check-for-update':
-      //   const {  } = await prompts(
-      //     {
-      //       type: 'confirm',
-      //       name: 'checkForLocalInstallationOnBoot',
-      //       message: 'During bootup checks if this app installed globally',
-      //       initial: config.state.checkForLocalInstallationOnBoot,
-      //     },
-      //     { onCancel: () => process.exit() }
-      //   );
-
-      //   config.update('checkForLocalInstallationOnBoot', checkForLocalInstallationOnBoot);
-      //   await this.run();
-      //   break;
 
       default:
         break;
