d69aff4f80e9fa1985fe7e091338176b77c70caa
Tahsin <tahsingenuies@gmail.com>
2021-08-25 00:00:07 +0700

    add dynamic mapper

--- src/operations/docker/actions/runAnImage.ts
+++ src/operations/docker/actions/runAnImage.ts
@@ -1,28 +1,26 @@
 import { BaseDockerAction } from '@app/operations/docker/baseDockerAction';
+import { askFuzzy } from '@app/util/myPrompts';
+import shellExecAsync from '@app/util/shellExecAsync';
 
 // d run --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=developer -d mysql
 const map = {
-  ['MySQL']: {
-    image: 'mysql',
-    containerName: 'mysql',
-    hostPort: '3306',
-    password: 'developer',
-  },
+  ['MySQL']: `docker run --name mysql-cross-tools -p 3306:3306 -e MYSQL_ROOT_PASSWORD=developer -d mysql:5.7`,
+  ['PostgreSQL']: `docker run --name postgres-cross-tools -p 5432:5432 -e POSTGRES_PASSWORD=developer -d postgres`,
 };
 
 class DockerAction extends BaseDockerAction {
   cmd = 'run-an-image';
 
   async handle() {
-    const image = 'mysql';
-    const data = await this.docker.searchImages(image);
-    console.log(data);
-    const option = map.MySQL;
+    const choices = Object.keys(map).map(key => ({
+      title: key,
+      value: key,
+    }));
+    const { operation } = await askFuzzy(choices, {});
+    console.log(operation);
 
-    // this.docker.run(option.image, [], process.stdout, )
-
-    // const info = await this.getDockerInfo();
-    // console.log(info);
+    // const image = 'MySQL';
+    // await shellExecAsync(map[image], {}, { loadingMsg: 'Initializing Container...' });
   }
 }
 
