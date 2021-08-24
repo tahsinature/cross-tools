dbb7200105a94371c51b6146c765be4abee9ef08
Tahsin <tahsingenuies@gmail.com>
2021-08-25 00:07:29 +0700

    add 2 boilerplates

--- src/operations/docker/actions/runAnImage.ts
+++ src/operations/docker/actions/runAnImage.ts
@@ -2,10 +2,25 @@ import { BaseDockerAction } from '@app/operations/docker/baseDockerAction';
 import { askFuzzy } from '@app/util/myPrompts';
 import shellExecAsync from '@app/util/shellExecAsync';
 
-// d run --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=developer -d mysql
 const map = {
-  ['MySQL']: `docker run --name mysql-cross-tools -p 3306:3306 -e MYSQL_ROOT_PASSWORD=developer -d mysql:5.7`,
-  ['PostgreSQL']: `docker run --name postgres-cross-tools -p 5432:5432 -e POSTGRES_PASSWORD=developer -d postgres`,
+  ['MySQL']: {
+    cmd: `docker run --name mysql-cross-tools -p 3306:3306 -e MYSQL_ROOT_PASSWORD=developer -d mysql:5.7`,
+    display: {
+      port: '3306',
+      user: 'root',
+      pass: 'developer',
+      version: 'mysql:5.7',
+    },
+  },
+  ['PostgreSQL']: {
+    cmd: `docker run --name postgres-cross-tools -p 5432:5432 -e POSTGRES_PASSWORD=developer -d postgres:alpine`,
+    display: {
+      port: '3306',
+      user: 'root',
+      pass: 'developer',
+      version: 'mysql:5.7',
+    },
+  },
 };
 
 class DockerAction extends BaseDockerAction {
@@ -16,11 +31,12 @@ class DockerAction extends BaseDockerAction {
       title: key,
       value: key,
     }));
-    const { operation } = await askFuzzy(choices, {});
-    console.log(operation);
 
-    // const image = 'MySQL';
-    // await shellExecAsync(map[image], {}, { loadingMsg: 'Initializing Container...' });
+    const { operation } = await askFuzzy(choices, {});
+    const { cmd, display } = (map as any)[operation];
+
+    await shellExecAsync(cmd, { silent: true });
+    console.log(display);
   }
 }
 
